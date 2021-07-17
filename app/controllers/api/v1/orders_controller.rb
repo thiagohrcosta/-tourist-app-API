c52c677b007a391dd1fc2abf243092f450e635d2
Thiago Costa <thiagohrcosta86@gmail.com>
2021-07-17 09:56:14 -0300

    add action show on controllers

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::OrdersController < Api::V1::BaseController
+  before_action :set_order, only: [:show]
+
   def index
     @orders = policy_scope(Order)
     authorize @orders
+  end
+
+  def show;end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+    authorize @order
   end
 end
