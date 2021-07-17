30521b1050805e5b7d8212aa8bcec68bd6aacf01
Thiago Costa <thiagohrcosta86@gmail.com>
2021-07-17 10:27:14 -0300

    Add update endpoint

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::OrdersController < Api::V1::BaseController
-  before_action :set_order, only: [:show]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_order, only: [:show, :update]
 
   def index
     @orders = policy_scope(Order)
@@ -8,10 +9,27 @@ class Api::V1::OrdersController < Api::V1::BaseController
 
   def show;end
 
+  def update
+    if @order.update(order_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_order
     @order = Order.find(params[:id])
     authorize @order
+  end
+
+  def order_params
+    params.require(:order).permit(:user_id, :ticket_id, :quantity)
+  end
+
+  def render_error
+    render json: { errors: @order.errors.full_messages},
+    status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :companies, only: [ :index, :show ]
-      resources :tickets, only: [ :index, :show ]
-      resources :orders, only: [ :index, :show ]
+      resources :companies, only: [ :index, :show, :update ]
+      resources :tickets, only: [ :index, :show, :update ]
+      resources :orders, only: [ :index, :show, :update ]
     end
   end
 end
